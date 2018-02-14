5c566496da2af2c537ab1f43926774238592c80e
sieon <sieon@outlook.com>
2018-02-14 21:25:43 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+            LeanTheme
+        </a>
+
+        <!-- Collapsed Hamburger -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#app-navbar-collapse" aria-controls="app-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
