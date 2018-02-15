28cafbcf429af983453b4d6013e6be4e8461956b
sieon <sieon@outlook.com>
2018-02-15 21:44:53 +0800

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
