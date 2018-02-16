a8876457252b4c72239fff92ada562941c60afc7
sieon <sieon@outlook.com>
2018-02-16 17:41:15 +0800

    Create categories

---
+++ database/migrations/2018_02_16_174647_seed_categories_data.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        => '未分类',
+                'description' => '',
+                'slug' => 'uncategorized',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
