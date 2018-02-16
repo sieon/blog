3aa0b1d31e977d5f04bb953eca65041b6ade2ee1
sieon <sieon@outlook.com>
2018-02-16 16:04:48 +0800

    User can upload avatar

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,8 +9,8 @@
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
         <div class="card">
             <div class="card-body">
-                <img class="img-fluid mb-3" src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600">
-                {{-- <hr> --}}
+                <img class="img-fluid mb-4" src="{{ $user->avatar }}">
+                <hr>
                 <h4><strong>个人简介</strong></h4>
                 <p>{{ $user->introduction }}</p>
                 <hr>
