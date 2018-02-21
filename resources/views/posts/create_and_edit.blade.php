edd7461c40ba4d0d241a8f7e5c8dab2625e0ee46
sieon <sieon@outlook.com>
2018-02-21 21:51:17 +0800

    fix editor layouts

--- resources/views/posts/create_and_edit.blade.php
+++ resources/views/posts/create_and_edit.blade.php
@@ -27,17 +27,24 @@
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 
-                <div class="form-group">
-                	<input class="form-control" type="text" name="title" id="title-field" value="{{ old('title', $post->title ) }}" placeholder="请填写标题" required/>
+                <div class="form-group row">
+                    <div class="col-sm-8">
+                        <input class="form-control" type="text" name="title" id="title-field" value="{{ old('title', $post->title ) }}" placeholder="请填写标题" required/>
+                    </div>
+                    <div class="col-sm-4">
+                        <select class="form-control" name="category_id" required>
+                            <option value="" hidden disabled {{ $post->id ? '' : 'selected' }}>请选择分类</option>
+                            @foreach ($categories as $value)
+                                <option value="{{ $value->id }}" {{ $post->category_id == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
+
                 <div class="form-group">
-                    <select class="form-control" name="category_id" required>
-                        <option value="" hidden disabled {{ $post->id ? '' : 'selected' }}>请选择分类</option>
-                        @foreach ($categories as $value)
-                            <option value="{{ $value->id }}" {{ $post->category_id == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
-                        @endforeach
-                    </select>
+                    <input class="form-control" type="text" name="slug" id="slug-field" value="{{ old('slug', $post->slug ) }}" placeholder="别名" required/>
                 </div>
+
                 <div class="form-group">
                 	<textarea name="content" id="content-field" class="form-control" rows="3" placeholder="正文，请填入至少三个字符的内容。" required>{{ old('content', $post->content ) }}</textarea>
                 </div>
@@ -45,10 +52,7 @@
                 	<label for="excerpt-field">Excerpt</label>
                 	<textarea name="excerpt" id="excerpt-field" class="form-control" rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('excerpt', $post->excerpt ) }}</textarea>
                 </div> --}}
-                <div class="form-group">
-                	<label for="slug-field">Slug</label>
-                	<input class="form-control" type="text" name="slug" id="slug-field" value="{{ old('slug', $post->slug ) }}" />
-                </div>
+
 
                     <div class="well well-sm">
                         <button type="submit" class="btn btn-primary">保存</button>
